a6cb5f40e82c438c190909c31fd1a7d74c0c3f36
JUANRV27 <rodriguezvelasquezjuanjose11@gmail.com>
2026-05-12 17:37:54 -0500

    Fixing file paths for local assets in figmaAssets.js

--- src/figmaAssets.js
+++ src/figmaAssets.js
@@ -1,15 +1,11 @@
-import imgImage6 from '../image.png'
-import imgImage7 from '../image 7.png'
-import imgImage9 from '../image 9.png'
-import imgImage31 from '../image 31.png'
-import imgShape from '../Shape.png'
-import imgFrame427318817 from '../Frame 427318817.png'
+import imgShapeSrc from '../Shape.png'
 
-export const imgIcon = imgShape
-export const imgShape = imgShape
-export const imgImage6 = imgImage6
-export const imgImage7 = imgImage7
-export const imgImage9 = imgImage9
-export const imgImage31 = imgImage31
-export const imgEllipse1 = imgShape
-export const imgFrame427318817 = imgFrame427318817
+export { default as imgImage6 } from '../image.png'
+export { default as imgImage7 } from '../image 7.png'
+export { default as imgImage9 } from '../image 9.png'
+export { default as imgImage31 } from '../image 31.png'
+export { default as imgFrame427318817 } from '../Frame 427318817.png'
+
+export const imgIcon = imgShapeSrc
+export const imgShape = imgShapeSrc
+export const imgEllipse1 = imgShapeSrc
