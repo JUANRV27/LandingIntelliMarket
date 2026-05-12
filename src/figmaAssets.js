bc828d462e38095d9c7e74409743769b6d80155e
JUANRV27 <rodriguezvelasquezjuanjose11@gmail.com>
2026-05-12 17:34:10 -0500

    refactor: update image imports and exports in figmaAssets.js

--- src/figmaAssets.js
+++ src/figmaAssets.js
@@ -1,10 +1,15 @@
-export const imgIcon = "Shape.png";
-export const imgShape = "Shape.png";
-export const imgImage6 = "image.png";
-export const imgImage7 = "image 7.png";
-export const imgImage9 = "image 9.png";
-export const imgImage31 = "image 31.png";
-export const imgEllipse1 = "Shape.png";
-export const imgVector200 = "Vector 200.png";
-export const imgVector201 = "Vector 201.png";
-export const imgFrame427318817 = "Frame 427318817.png";
+import imgImage6 from '../image.png'
+import imgImage7 from '../image 7.png'
+import imgImage9 from '../image 9.png'
+import imgImage31 from '../image 31.png'
+import imgShape from '../Shape.png'
+import imgFrame427318817 from '../Frame 427318817.png'
+
+export const imgIcon = imgShape
+export const imgShape = imgShape
+export const imgImage6 = imgImage6
+export const imgImage7 = imgImage7
+export const imgImage9 = imgImage9
+export const imgImage31 = imgImage31
+export const imgEllipse1 = imgShape
+export const imgFrame427318817 = imgFrame427318817
